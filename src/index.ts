d3c1d5737940ba818b4278d35b977e579adb76df
Fluturecode <fluturecode@gmail.com>
2023-06-25 22:31:32 -0400

    added @solana/spl-token

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import { initializeKeypair } from "./initializeKeypair"
 import * as web3 from "@solana/web3.js"
-
+import * as token from "@solana/spl-token"
 async function main() {
   const connection = new web3.Connection(web3.clusterApiUrl("devnet"))
   const user = await initializeKeypair(connection)
+
+
 }
 
 main()
