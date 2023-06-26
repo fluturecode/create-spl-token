401636f0c8cb9db545a92ec50b7aa19b1b16aa06
Fluturecode <fluturecode@gmail.com>
2023-06-25 22:35:55 -0400

    added createTokenAccount

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,60 @@
 import { initializeKeypair } from "./initializeKeypair"
 import * as web3 from "@solana/web3.js"
 import * as token from "@solana/spl-token"
+async function createNewMint(
+  connection: web3.Connection,
+  payer: web3.Keypair,
+  mintAuthority: web3.PublicKey,
+  freezeAuthority: web3.PublicKey,
+  decimals: number
+): Promise<web3.PublicKey> {
+
+  const tokenMint = await token.createMint(
+      connection,
+      payer,
+      mintAuthority,
+      freezeAuthority,
+      decimals
+  );
+
+  console.log(
+      `Token Mint: https://explorer.solana.com/address/${tokenMint}?cluster=devnet`
+  );
+
+  return tokenMint;
+}
+
+async function createTokenAccount(
+    connection: web3.Connection,
+    payer: web3.Keypair,
+    mint: web3.PublicKey,
+    owner: web3.PublicKey
+) {
+    const tokenAccount = await token.getOrCreateAssociatedTokenAccount(
+        connection,
+        payer,
+        mint,
+        owner
+    )
+
+    console.log(
+        `Token Account: https://explorer.solana.com/address/${tokenAccount.address}?cluster=devnet`
+    )
+
+    return tokenAccount
+}
+
 async function main() {
   const connection = new web3.Connection(web3.clusterApiUrl("devnet"))
   const user = await initializeKeypair(connection)
 
+  const mint = await createNewMint(
+      connection,
+      user,
+      user.publicKey,
+      user.publicKey,
+      2
+  )
 
+  const mintInfo = await token.getMint(connection, mint);
 }
-
-main()
-  .then(() => {
-    console.log("Finished successfully")
-    process.exit(0)
-  })
-  .catch((error) => {
-    console.log(error)
-    process.exit(1)
-  })
